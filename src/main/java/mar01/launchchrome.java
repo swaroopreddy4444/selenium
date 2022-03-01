08484795e218f71f96a6ee433c8fa2479facd26b
swaroopreddy4444 <swaroopreddy4444@gmail.com>
2022-03-01 09:03:11 +0530

    launch chrome successful

---
+++ src/main/java/mar01/launchchrome.java
@@ -0,0 +1,22 @@
+package mar01;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class launchchrome {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver","C://selenium_driver/chromedriver_win32/chromedriver.exe");
+		//WebDriverManager.chromedriver().setup();
+		
+		WebDriver wb =new ChromeDriver();
+		
+		wb.get("https://mail.google.com");
+	//	wb.findElement(By.xpath(null))
+	}
+
+}
