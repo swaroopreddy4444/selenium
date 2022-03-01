a3ed86018fcbade8d2d8e84a5f8c2e1d22fdf26d
swaroopreddy4444 <swaroopreddy4444@gmail.com>
2022-03-01 09:10:06 +0530

    input username

--- src/main/java/mar01/launchchrome.java
+++ src/main/java/mar01/launchchrome.java
@@ -10,13 +10,14 @@ public class launchchrome {
 
 	public static void main(String[] args) {
 
-		System.setProperty("webdriver.chrome.driver","C://selenium_driver/chromedriver_win32/chromedriver.exe");
-		//WebDriverManager.chromedriver().setup();
+	//	System.setProperty("webdriver.chrome.driver","C://selenium_driver/chromedriver_win32/chromedriver.exe");
+		WebDriverManager.chromedriver().setup();
 		
 		WebDriver wb =new ChromeDriver();
 		
 		wb.get("https://mail.google.com");
-	//	wb.findElement(By.xpath(null))
+		wb.findElement(By.id("identifierId")).sendKeys("swaroopeddy4444");
+		
 	}
 
 }
