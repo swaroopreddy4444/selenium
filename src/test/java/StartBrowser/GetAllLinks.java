cfb33646f476d2e255079f95a7a06d6dbebae399
swaroopreddy4444 <swaroopreddy4444@gmail.com>
2022-04-15 10:53:33 +0530

    get all links

---
+++ src/test/java/StartBrowser/GetAllLinks.java
@@ -0,0 +1,34 @@
+package StartBrowser;
+
+import java.time.Duration;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GetAllLinks {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+		
+		driver.get("https://www.google.co.in/");
+		
+		List<WebElement> links=driver.findElements(By.tagName("a"));
+		
+		System.out.println(links.size());
+		
+		for(WebElement link:links) {
+			System.out.println(link.getText()+"---\t"+link.getAttribute("href"));	
+		}
+		
+	}
+
+}
