0ab0390882a068d1fbc2450b8c53d9c119324327
swaroopreddy4444 <swaroopreddy4444@gmail.com>
2022-05-19 11:03:38 +0530

    robot class file upload
    guava dependencies update in POM.xml file

--- src/test/java/Udemy_Assignments/UploadFileTest.java
+++ src/test/java/Udemy_Assignments/UploadFileTest.java
@@ -12,7 +12,7 @@ public class UploadFileTest extends SelectBrowser{
 		
 
 		driver.get("https://demo.guru99.com/test/upload/");
-		driver.findElement(By.xpath("//*[@id=\"uploadfile_0\"]")).sendKeys("C:\\Users\\swaroop reddy\\Desktop\\Document2.pdf");
+		driver.findElement(By.xpath("//*[@id=\"uploadfile_0\"]")).sendKeys("C:\\Users\\Parigi-2-DA\\Documents\\IMG_20220104_0002.jpg");
 		
 //		driver.get("https://www.remove.bg/upload");
 //		driver.findElement(By.xpath("//*[@id=\"page-content\"]/div[1]/div/div/div[1]/div/div[1]/div[2]/button")).sendKeys("C:\\Users\\swaroop reddy\\Desktop\\peronsal\\sad.jpeg");
